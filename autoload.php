4478b84916b9d4fb946a6d007b7120053e9a29a4
Marcus <marcusds@users.noreply.github.com>
2015-02-19 18:24:53 -0800

    Put braces where I want them.

--- autoload.php
+++ autoload.php
@@ -6,13 +6,11 @@
 function stripe_api_autoload($class_name) {
 	$name_prefix = 'NAMESPACE\\'; // The namespace prefix
 	$base = __DIR__ . '/SUBDIRECTORY/'; // Directory for the namespace
-	if(strpos($class_name, $name_prefix) != 0) // Check if the class belongs in the namespace
-	{
+	if(strpos($class_name, $name_prefix) != 0) { // Check if the class belongs in the namespace
 		return; // Does not belong to the namespace, skip
 	}
 	$class_file = $base . str_replace('\\', '/', substr($class_name, strlen($name_prefix))) . '.php'; // Get the unprefixed class's file
-	if(file_exists($class_file)) // Check if the file exists
-	{
+	if(file_exists($class_file)) { // Check if the file exists
 		require $class_file;
 	}
 }
